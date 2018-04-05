b0e00a23467dc29c331c576e38bba8ed7f9eef05
Daniel Carl Jones <dcjones1@sheffield.ac.uk>
2018-04-05 14:51:11 +0300

    Add debouncing status

--- components/CalculatorForm.vue
+++ components/CalculatorForm.vue
@@ -11,7 +11,7 @@
           </b-form-group>
         </b-col>
         <b-col v-else-if="block.type === 'result'">
-          <b-form-group :label="block.label">
+          <b-form-group :label="block.label" :description="statusCaption">
             <b-form-input plaintext :value="valueOrPlaceholder(resultBlockValues[block.id])"></b-form-input>
           </b-form-group>
         </b-col>
@@ -36,19 +36,28 @@ export default {
         acc[id] = undefined;
         return acc;
       }, {}),
-      formData: {}
+      formData: {},
+      dirty: false,
+      fetching: false
     };
   },
   methods: {
     valueOrPlaceholder (value, placeholder = '-') {
       return value === undefined ? placeholder : value;
     },
+    onChange () {
+      this.dirty = true;
+      this.calculate();
+    },
     calculate: debounce(function () {
+      this.fetching = true;
       return axios.post(`/api/calculators/${this.calculator._id}/calculate`, { formData: this.formData })
         .then(res => {
           Object.entries(res.data.results).forEach(([key, value]) => {
             this.resultBlockValues[key] = value;
           });
+          this.fetching = false;
+          this.dirty = false;
         })
         .catch(console.error);
     }, 1000)
@@ -56,10 +65,21 @@ export default {
   components: {
     MultiOptionInput
   },
+  computed: {
+    statusCaption () {
+      if (this.fetching) {
+        return 'Calculating... ⟳';
+      } else if (this.dirty) {
+        return 'Waiting for you to finish...';
+      } else {
+        return 'Done.';
+      }
+    }
+  },
   watch: {
     formData: {
       deep: true,
-      handler () { this.calculate(); }
+      handler () { this.onChange(); }
     }
   }
 };
