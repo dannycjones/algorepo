f81fdafc843c832c32842f064bd3a0be4a91dcf4
Daniel Carl Jones <dcjones1@sheffield.ac.uk>
2018-04-18 11:56:37 +0100

    Update calculator model

--- models/Calculator.js
+++ models/Calculator.js
@@ -1,18 +1,57 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-const schema = new Schema({
+const ruleSchema = new Schema({
+  input: {
+    required: true,
+    type: String
+  },
+  output: {
+    required: true,
+    type: {}
+  },
+  value: {
+    required: true,
+    type: String
+  },
+  comparator: {
+    required: true,
+    type: String
+  }
+});
+
+const contentSchema = new Schema({
+  rules: [ruleSchema],
+  formula: String
+});
+
+const blockSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  type: String,
+  content: {},
+  label: String,
+  display: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const calculatorSchema = new Schema({
   name: {
     type: String,
     required: true
   },
   description: String,
-  blocks: [],
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  blocks: [blockSchema],
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 }, {
   timestamps: true
 });
 
-const Calculator = mongoose.model('Calculator', schema);
+const Calculator = mongoose.model('Calculator', calculatorSchema);
 
 export default Calculator;
